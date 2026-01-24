0dca2873367c447dc33c45af3944dacea68b0cad
00Goldy <damien_girard@live.fr>
2026-01-24 16:04:13 +0100

    Modifying Plot function to save plot and adding description for variables

--- notebooks/Plot.py
+++ notebooks/Plot.py
@@ -1,5 +1,30 @@
 import matplotlib.pyplot as plt
-def Plot(H1, H2=None, labelH1=None, labelH2=None, xlabel=None, ylabel=None, title=None, logx=False, logy=False, ratio=False):
+
+def Plot(H1, H2=None, 
+         labelH1=None, labelH2=None, 
+         labelx=None, labely=None,
+         title=None, 
+         logx=False, logy=False, 
+         ratio=False,
+         save=False, savename=None):
+    """
+    Take histograms and plot the figure desired
+    :param: example: This is an example of a parameter || format: boolean
+
+    :param: H1: First Histogram || format: hist.Hist
+    :param: H2: Second Histogram || format: hist.Hist
+    :param: labelH1: Name of what data H1 contains, with eventual cuts || format: string
+    :param: labelH2: Name of what data H2 contains, with eventual cuts || format: string
+    :param: labelx: Name of the x axis || format: string
+    :param: labely: Name of the y axis || format: string
+    :param: title: Title of the plot || format: string
+    :param: logx: Define if the x axis should be log-ed || format: boolean
+    :param: logy: Define if the y axis should be log-ed || format: boolean
+    :param: ration: Define if the 2 histograms should be ratioed (H1/H2) || format: boolean
+    :param: save: Define if the plot should be saved || format: boolean
+    :param: savename: Name of the saved plot || format: string
+
+    """
     plt.figure(figsize=(10, 6))
     
     if ratio:
@@ -8,7 +33,7 @@ def Plot(H1, H2=None, labelH1=None, labelH2=None, xlabel=None, ylabel=None, titl
         default_ylabel = "Ratio"
     else:
         H1.plot(label=labelH1)
-        if H2 is not None:
+        if H2:
             H2.plot(label=labelH2)
         default_ylabel = "# of tracks"
     
@@ -17,9 +42,21 @@ def Plot(H1, H2=None, labelH1=None, labelH2=None, xlabel=None, ylabel=None, titl
     if logy:
         plt.yscale("log")
     
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel if ylabel else default_ylabel)
-    plt.title(title if title else "Histogram Plot")
+    plt.xlabel(labelx)
+    plt.ylabel(labely if labely else default_ylabel)
+    plt.title(title if title else "Ratio" if ratio else "Histogram Plot")
     plt.grid(color='green', linestyle='--', linewidth=0.5)
     plt.legend()
+
+    if save:
+        if savename:
+            plt.savefig(savename+'.png')
+        else:
+            plt.savefig('YourAwesomePlot.png')
+
     plt.show()
+
+
+
+
+
